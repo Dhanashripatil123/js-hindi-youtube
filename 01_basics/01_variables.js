a920ccbaa991233c0350e1279b5479075bf88c33
Dhanashripatil123 <150023651+Dhanashripatil123@users.noreply.github.com>
2024-08-08 17:56:00 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId=1445533
+let accountEmail="dhanadhl@google.com"
+var accountpassword="12345"
+accouncity="jaipur"
+let accountState;
+
+
+// accountId=2 not allowed//
+
+accountEmail="hc.@gmail.com"
+accountpassword="3323542365"
+accouncity="dhule"
+
+
+// console.log(accountId);//
+
+console.table([accountpassword,accountEmail,accountId,accouncity,accountState])
+
+/*
+prefer not use var
+because of issue in block scopeand functional scope
+*/
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strick";//treat all js code as newer version
+
+// alert(3+3) we are using node.js not browser //
+
+console.log(3+
+    
+    3)//code readability should bo high
+console.log("dhanashri")
+
+
+let name = "dhanashri"
+let age = 18
+let isLoggedIn = false
+
+//number ==> 2 to power 53
+//bigint
+//string => ""
+//boolen => true/false
+//null ==> standalone value
+//undefined ==> 
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object
+
+
+
