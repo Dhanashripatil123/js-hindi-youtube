f7aafb1e1d35f6f3ea11ca50944fddc2dd360724
Dhanashripatil123 <150023651+Dhanashripatil123@users.noreply.github.com>
2024-08-09 18:20:35 +0000

    conversion of datatype

---
+++ 01_basics/03_conversiononOperation.js
@@ -0,0 +1,25 @@
+let score = "dhanshri"
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueINnumber = number (score)
+//console.log(typeof valueINnumber);
+//console.log(valueINnumber);//
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "hitesh" => true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+
+
+
